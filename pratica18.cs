3d6ab00bf688373a937bb6dd58a3a81009eeed69
João Vitor Machado Almeida <joaovitormachado.developer@gmail.com>
2024-05-08 07:26:49 -0300

    Update pratica18.cs
    Adicionei o System.Globalization e criei um metodo para o calculo da area do triangulo

--- pratica18.cs
+++ pratica18.cs
@@ -1,9 +1,16 @@
-
 using System;
+using System.Globalization;
 class Triangulo{
     public double A;
     public double B;
     public double C;
+    
+    public double Area(){
+    
+    double p = (A + B + C) / 2;
+     return Math.Sqrt(p*(p - A) * (p - B) * (p - C));
+    
+    }
 }
 class HelloWorld {
   static void Main() {
@@ -11,20 +18,19 @@ class HelloWorld {
       Triangulo y = new Triangulo();
       
     Console.WriteLine("Entre com a medida do trinagulo x:");
-     x.A = double.Parse(Console.ReadLine());
-     x.B = double.Parse(Console.ReadLine());
-     x.C = double.Parse(Console.ReadLine());
+     x.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+     x.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+     x.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
     
     Console.WriteLine("Entre com a medida do triangulo y:");
-     y.A = double.Parse(Console.ReadLine());
-     y.B = double.Parse(Console.ReadLine());
-     y.C = double.Parse(Console.ReadLine());
+     y.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+     y.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+     y.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
     
-    double p = (x.A + x.B + x.C) / 2;
-     double areaX = Math.Sqrt(p*(p - x.A) * (p - x.B) * (p - x.C));
     
-    p = (y.A + y.B + y.C) / 2;
-    double areaY = Math.Sqrt(p*(p - y.A) * (p - y.B) * (p - y.C));
+    double areaX = x.Area();
+    
+    double areaY = y.Area();
     
     Console.WriteLine("Área de x = {0:F4}",areaX);
     Console.WriteLine("Área de y = {0:F4}",areaY);
